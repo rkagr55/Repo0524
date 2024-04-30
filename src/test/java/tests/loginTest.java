dfac3a5bb67bc864835690938b04b501494ad379
rkagr55 <rkagr55@gmail.com>
2024-05-01 02:26:38 +0530

    first commit message

---
+++ src/test/java/tests/loginTest.java
@@ -0,0 +1,11 @@
+package tests;
+import org.testng.annotations.Test;
+
+public class loginTest {
+
+@Test
+public void login() {
+	System.out.println("Hello World");
+}
+}
+//https://www.youtube.com/watch?v=99weHh_Ufws
